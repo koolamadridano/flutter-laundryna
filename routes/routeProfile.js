411e8dfe678b07207ba57cba38aea646257b357c
Kola <kolya.madridano@gmail.com>
2022-03-27 18:35:16 +0800

    #032722 pre-release

---
+++ routes/routeProfile.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+
+const profile = require("../controllers/profile/profileController");
+const protected = require("../middleware/authentication");
+
+// PROFILE
+router.post("/profile", (req, res) => 
+    profile.createProfile(req, res)
+);
+router.get("/profile", (req, res) => 
+    profile.getAllProfiles(req, res)
+);
+router.get("/profile/:id", (req, res) => 
+    profile.getProfile(req, res)
+);
+router.put("/profile/:id", (req, res) => 
+    profile.updateProfile(req, res)
+);
+router.delete("/profile/:id", (req, res) => 
+    profile.deleteProfile(req, res)
+);
+
+router.post("/a/profile", (req, res) => 
+    profile.updateCustomerAvatar(req, res)
+);
+
+router.post("/b/profile", (req, res) => 
+    profile.updateLaundryBanner(req, res)
+);
+
+module.exports = router;
